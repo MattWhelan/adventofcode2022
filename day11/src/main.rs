b56c0ed424d8a6ae6fbecebc523efa8d4bad8586
Matt Whelan <mwhelan@newrelic.com>
2022-12-12 09:36:34 -0800

    Days 11 and 12
    Could be cleaner, but they work.

---
+++ day11/src/main.rs
@@ -0,0 +1,212 @@
+use std::cell::{RefCell};
+use anyhow::{Error, Result};
+use std::str::FromStr;
+use itertools::Itertools;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+#[derive(Debug, Copy, Clone)]
+enum Op {
+    Add(usize),
+    Mul(usize),
+    Square
+}
+
+impl Op {
+    fn exec(&self, old: usize) -> usize {
+        match self {
+            Op::Add(n) => old + n,
+            Op::Mul(n) => old * n,
+            Op::Square => old * old,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Monkey {
+    id: usize,
+    items: Vec<usize>,
+    op: Op,
+    modulus: usize,
+    true_target: usize,
+    false_target: usize
+}
+
+impl FromStr for Monkey {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static!{
+            static ref RE: Regex = Regex::new(r#"Monkey (\d+):
+  Starting items: ([\d, ]+)
+  Operation: new = old (.) (\d+|old)
+  Test: divisible by (\d+)
+    If true: throw to monkey (\d+)
+    If false: throw to monkey (\d+)"#).unwrap();
+        }
+
+        if let Some(caps) = RE.captures(s) {
+            let items = caps[2].split(", ").map(|item_str| item_str.parse().unwrap()).collect();
+            let op = if let Ok(n) = caps[4].parse() {
+                match &caps[3] {
+                    "*" => Op::Mul(n),
+                    "+" => Op::Add(n),
+                    _ => unreachable!(),
+                }
+            } else {
+                Op::Square
+            };
+
+            Ok(Monkey {
+                id: caps[1].parse().unwrap(),
+                items,
+                op,
+                modulus: caps[5].parse().unwrap(),
+                true_target: caps[6].parse().unwrap(),
+                false_target: caps[7].parse().unwrap(),
+            })
+        } else {
+            Err(Error::msg("Didn't match"))
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Forest {
+    monkeys: Vec<RefCell<Monkey>>,
+    inspection_counts: Vec<usize>,
+    very_worried: bool,
+    test_modulus: usize,
+}
+
+impl Forest {
+    fn new(monkeys: &[Monkey], very_worried: bool) -> Forest {
+        let test_modulus = monkeys.iter().map(|m| m.modulus).product();
+
+        Forest {
+            inspection_counts: vec![0; monkeys.len()],
+            monkeys: monkeys.into_iter().map(|m| RefCell::new(m.clone())).collect(),
+            very_worried,
+            test_modulus
+        }
+    }
+
+    fn round(&mut self) {
+        for i in 0..self.monkeys.len() {
+            self.turn(i);
+        }
+    }
+
+    fn turn(&mut self, id: usize) {
+        let mut m = self.monkeys[id].borrow_mut();
+        let mut t = self.monkeys[m.true_target].borrow_mut();
+        let mut f = self.monkeys[m.false_target].borrow_mut();
+        for item in m.items.iter() {
+            //inspect
+            self.inspection_counts[id] += 1;
+            let mut new_worry = m.op.exec(*item);
+            if self.very_worried {
+                new_worry = new_worry % self.test_modulus;
+            } else {
+                new_worry = new_worry / 3;
+            }
+            //test
+            if new_worry % m.modulus == 0 {
+                //throw
+                t.items.push(new_worry);
+            } else {
+                //throw
+                f.items.push(new_worry);
+            }
+        }
+        m.items.clear();
+    }
+
+    fn monkey_business(&self) -> usize {
+        self.inspection_counts.iter()
+            .sorted()
+            .rev()
+            .take(2)
+            .product()
+    }
+}
+
+fn main() -> Result<()> {
+    let input: Vec<Monkey> = INPUT.split("\n\n").map(|l| l.parse().unwrap()).collect();
+
+    {
+        let mut forest = Forest::new(&input, false);
+
+        for _ in 0..20 {
+            forest.round();
+        }
+
+        println!("Part 1: {}", forest.monkey_business());
+    }
+    {
+        let mut forest = Forest::new(&input, true);
+
+        for _ in 0..10000 {
+            forest.round();
+        }
+
+        println!("Part 2: {}", forest.monkey_business());
+    }
+    Ok(())
+}
+
+const INPUT: &str = r#"Monkey 0:
+  Starting items: 57, 58
+  Operation: new = old * 19
+  Test: divisible by 7
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 66, 52, 59, 79, 94, 73
+  Operation: new = old + 1
+  Test: divisible by 19
+    If true: throw to monkey 4
+    If false: throw to monkey 6
+
+Monkey 2:
+  Starting items: 80
+  Operation: new = old + 6
+  Test: divisible by 5
+    If true: throw to monkey 7
+    If false: throw to monkey 5
+
+Monkey 3:
+  Starting items: 82, 81, 68, 66, 71, 83, 75, 97
+  Operation: new = old + 5
+  Test: divisible by 11
+    If true: throw to monkey 5
+    If false: throw to monkey 2
+
+Monkey 4:
+  Starting items: 55, 52, 67, 70, 69, 94, 90
+  Operation: new = old * old
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 3
+
+Monkey 5:
+  Starting items: 69, 85, 89, 91
+  Operation: new = old + 7
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 7
+
+Monkey 6:
+  Starting items: 75, 53, 73, 52, 75
+  Operation: new = old * 7
+  Test: divisible by 2
+    If true: throw to monkey 0
+    If false: throw to monkey 4
+
+Monkey 7:
+  Starting items: 94, 60, 79
+  Operation: new = old + 2
+  Test: divisible by 3
+    If true: throw to monkey 1
+    If false: throw to monkey 6"#;
---
+++ day12/src/main.rs
@@ -0,0 +1,214 @@
+use std::collections::{HashMap, HashSet};
+use std::ops::Index;
+use anyhow::Result;
+
+#[derive(Debug)]
+struct Map {
+    heights: Vec<Vec<u32>>,
+    start: [usize; 2],
+    end: [usize; 2],
+}
+
+impl Map {
+    fn new(map_chars: &Vec<Vec<char>>) -> Map {
+        let heights = map_chars.iter().map(|row| {
+            row.iter().map(|ch| match ch {
+                'S' => 0,
+                'E' => 25,
+                _ => u32::from(*ch) - u32::from('a'),
+            }).collect()
+        }).collect();
+
+        let start= map_chars.iter().enumerate().flat_map(|(y, row)| {
+            row.iter().enumerate().filter_map(move |(x, ch)| match ch {
+                'S' => Some([x, y]),
+                _ => None,
+            })
+        }).next().unwrap();
+
+        let end= map_chars.iter().enumerate().flat_map(|(y, row)| {
+            row.iter().enumerate().filter_map(move |(x, ch)| match ch {
+                'E' => Some([x, y]),
+                _ => None,
+            })
+        }).next().unwrap();
+
+        Map {
+            heights,
+            start,
+            end
+        }
+    }
+
+    fn neighbors(&self, pt: &[usize; 2]) -> impl Iterator<Item=[usize; 2]> {
+        let mut left = None;
+        let mut right = None;
+        let mut up = None;
+        let mut down = None;
+
+        let [x, y] = pt;
+
+        if *x > 0 && self[pt]+1 >= self[&[x-1, *y]] {
+            left = Some([x-1, *y]);
+        }
+        if *x + 1 < self.heights[*y].len() && self[pt]+1 >= self[&[x+1, *y]] {
+            right = Some([x+1, *y]);
+        }
+        if *y > 0 && self[pt]+1 >= self[&[*x, y-1]] {
+            up = Some([*x, y-1]);
+        }
+        if *y + 1 < self.heights.len() && self[pt]+1 >= self[&[*x, y+1]] {
+            down = Some([*x, y+1]);
+        }
+
+        [left, right, up, down].into_iter().filter_map(|x|x)
+    }
+
+    fn neighbors_2(&self, pt: &[usize; 2]) -> impl Iterator<Item=[usize; 2]> {
+        let mut left = None;
+        let mut right = None;
+        let mut up = None;
+        let mut down = None;
+
+        let [x, y] = pt;
+
+        if *x > 0 && self[pt] <= self[&[x-1, *y]]+1{
+            left = Some([x-1, *y]);
+        }
+        if *x + 1 < self.heights[*y].len() && self[pt] <= self[&[x+1, *y]]+1 {
+            right = Some([x+1, *y]);
+        }
+        if *y > 0 && self[pt] <= self[&[*x, y-1]]+1 {
+            up = Some([*x, y-1]);
+        }
+        if *y + 1 < self.heights.len() && self[pt] <= self[&[*x, y+1]]+1 {
+            down = Some([*x, y+1]);
+        }
+
+
+        [left, right, up, down].into_iter().filter_map(|x|x)
+    }
+
+    fn dijkstra(&self) -> u32 {
+        let mut visited = HashSet::new();
+        let mut distance: HashMap<[usize; 2], u32> = HashMap::new();
+        let mut current = self.start;
+
+        distance.insert(current.clone(), 0);
+
+        while !visited.contains(&self.end) {
+            let neighbor_dist = distance[&current] + 1;
+            self.neighbors(&current)
+                .filter(|p| !visited.contains(p))
+                .for_each(|p| {
+                    let d = distance.entry(p).or_insert(neighbor_dist);
+                    if *d > neighbor_dist {
+                        *d = neighbor_dist;
+                    }
+                });
+            visited.insert(current.clone());
+
+            current = distance.iter().filter(|(p, _)| !visited.contains(&**p))
+                .min_by_key(|(_, d)| *d)
+                .unwrap()
+                .0
+                .clone()
+        }
+
+        distance[&self.end]
+    }
+
+    fn dijkstra_2(&self) -> u32 {
+        let mut visited = HashSet::new();
+        let mut distance: HashMap<[usize; 2], u32> = HashMap::new();
+        let mut current = self.end;
+
+        distance.insert(current.clone(), 0);
+
+        loop {
+            let neighbor_dist = distance[&current] + 1;
+            self.neighbors_2(&current)
+                .filter(|p| !visited.contains(p))
+                .for_each(|p| {
+                    let d = distance.entry(p).or_insert(neighbor_dist);
+                    if *d > neighbor_dist {
+                        *d = neighbor_dist;
+                    }
+                });
+            visited.insert(current.clone());
+
+            if self[&current] == 0 {
+                break distance[&current]
+            }
+
+            current = distance.iter().filter(|(p, _)| !visited.contains(&**p))
+                .min_by_key(|(_, d)| *d)
+                .unwrap()
+                .0
+                .clone()
+        }
+    }
+}
+
+impl Index<&[usize; 2]> for Map {
+    type Output = u32;
+
+    fn index(&self, [x, y]: &[usize; 2]) -> &Self::Output {
+        &self.heights[*y][*x]
+    }
+}
+
+fn main() -> Result<()> {
+    let input: Vec<Vec<char>> = INPUT.lines().map(|l| l.chars().collect()).collect();
+
+    let map = Map::new(&input);
+
+    let path_len = map.dijkstra();
+    println!("Part 1: {}", path_len);
+    let path_len_2 = map.dijkstra_2();
+    println!("Part 2: {}", path_len_2);
+
+    Ok(())
+}
+
+const INPUT: &str = r#"abcccccccaaaaaaaaccccccccccaaaaaaccccccaccaaaaaaaccccccaacccccccccaaaaaaaaaaccccccccccccccccccccccccccccccccaaaaa
+abcccccccaaaaaaaaacccccccccaaaaaacccccaaacaaaaaaaaaaaccaacccccccccccaaaaaaccccccccccccccccccccccccccccccccccaaaaa
+abcccccccaaaaaaaaaaccccccccaaaaaacaaacaaaaaaaaaaaaaaaaaaccccccccccccaaaaaaccccccccccccccaaacccccccccccccccccaaaaa
+abaaacccccccaaaaaaacccccccccaaacccaaaaaaaaaaaaaaaaaaaaaaaaacccccccccaaaaaaccccccccccccccaaacccccccccccccccccaaaaa
+abaaaaccccccaaaccccccccccccccccccccaaaaaaaaacaaaacacaaaaaacccccccccaaaaaaaacccccccccccccaaaaccaaacccccccccccaccaa
+abaaaaccccccaaccccaaccccccccccccccccaaaaaaacaaaaccccaaaaaccccccccccccccccacccccccccccccccaaaaaaaaacccccccccccccca
+abaaaaccccccccccccaaaacccccccccaacaaaaaaaacccaaacccaaacaacccccccccccccccccccccccccccciiiiaaaaaaaacccccccccccccccc
+abaaacccccccccccaaaaaacccccccccaaaaaaaaaaacccaaacccccccaacccccccccccaacccccccccccccciiiiiiijaaaaccccccccaaccccccc
+abaaaccccccccccccaaaacccccccccaaaaaaaacaaacccaaaccccccccccccccccccccaaacaaacccccccciiiiiiiijjjacccccccccaaacccccc
+abcccccaacaacccccaaaaaccccccccaaaaaacccccacaacccccccccccccccccccccccaaaaaaaccccccciiiinnnoijjjjjjjjkkkaaaaaaacccc
+abcccccaaaaacccccaacaaccccccccccaaaacccaaaaaaccccccccccccccccccccccccaaaaaaccccccciiinnnnooojjjjjjjkkkkaaaaaacccc
+abccccaaaaacccccccccccccccccccccaccccccaaaaaaaccccccccccccccccccccaaaaaaaaccccccchhinnnnnoooojjooopkkkkkaaaaccccc
+abccccaaaaaaccccccccccccccccccccccccccccaaaaaaacccccccccccccccccccaaaaaaaaacccccchhhnnntttuooooooopppkkkaaaaccccc
+abccccccaaaaccccccccccacccccccccccccccccaaaaaaacccaaccccccccccccccaaaaaaaaaaccccchhhnnttttuuoooooppppkkkaaaaccccc
+abccccccaccccccccccccaaaacaaaccccccccccaaaaaacaaccaacccaaccccccccccccaaacaaacccchhhnnnttttuuuuuuuuupppkkccaaccccc
+abccccccccccccccaaccccaaaaaaaccccccccccaaaaaacaaaaaacccaaaaaaccccccccaaacccccccchhhnnntttxxxuuuuuuupppkkccccccccc
+abcccccccccccccaaaacccaaaaaaacccaccccccccccaaccaaaaaaacaaaaaaccccccccaacccaaccchhhhnnnttxxxxuuyyyuupppkkccccccccc
+abcccccccccccccaaaaccaaaaaaaaacaaacccccccccccccaaaaaaaaaaaaaccccccccccccccaaachhhhmnnnttxxxxxxyyyuvppkkkccccccccc
+abcccccccccccccaaaacaaaaaaaaaaaaaaccccccccccccaaaaaacaaaaaaaccccccccccccccaaaghhhmmmttttxxxxxyyyyvvpplllccccccccc
+abccacccccccccccccccaaaaaaaaaaaaaaccccccccccccaaaaaacccaaaaaacccaacaacccaaaaagggmmmttttxxxxxyyyyvvppplllccccccccc
+SbaaaccccccccccccccccccaaacaaaaaaaacccccccccccccccaacccaaccaacccaaaaacccaaaagggmmmsttxxxEzzzzyyvvvppplllccccccccc
+abaaaccccccccccccccccccaaaaaaaaaaaaacaaccccccccccccccccaaccccccccaaaaaccccaagggmmmsssxxxxxyyyyyyvvvqqqlllcccccccc
+abaaacccccccccccccccccccaaaaaaaaaaaaaaaaacccccccccccccccccccccccaaaaaaccccaagggmmmsssxxxwywyyyyyyvvvqqlllcccccccc
+abaaaaacccccccccccccccccccaacaaaccaaaaaaacccccccccccccccccccccccaaaaccccccaagggmmmssswwwwwyyyyyyyvvvqqqllcccccccc
+abaaaaaccccccccccccccccccccccaaaccccaaaacccccccccccccccccaaccaacccaaccccccccgggmmmmssssswwyywwvvvvvvqqqlllccccccc
+abaaaaacccccccccccccaccacccccaaaccccaaaacccccccccccccccccaaaaaacccccccccccaaggggmllllsssswwywwwvvvvqqqqlllccccccc
+abaaccccccccccccccccaaaaccccccccccccaccaccccccccccccccccccaaaaacccccccccccaaagggglllllssswwwwwrrqqqqqqmmllccccccc
+abaaccccccccccccccccaaaaaccccccaaccaaccccccccccccccccccccaaaaaaccaacccccccaaaaggfffllllsswwwwrrrrqqqqqmmmcccccccc
+abacaaaccccccccccccaaaaaaccccccaaaaaaccccccaacccccccccccaaaaaaaacaaacaaccccaaaaffffflllsrrwwwrrrmmmmmmmmmcccccccc
+abaaaaaccccccccccccaaaaaaccccccaaaaaccccccaaaaccccccccccaaaaaaaacaaaaaaccccaaaaccfffflllrrrrrrkkmmmmmmmccccaccccc
+abaaaacccccccccccccccaaccccccccaaaaaacccccaaaacccccccccccccaaccaaaaaaaccccccccccccffflllrrrrrkkkmmmmmccccccaccccc
+abaaacccccccccccccccccccccccccaaaaaaaaccccaaaacccccccccccccaaccaaaaaaacccccccccccccfffllkrrrkkkkmddddcccccaaacccc
+abaaacccccccccccccccccccccccccaaaaaaaacccccccccccccccccccccccccccaaaaaaccccccccccccfffllkkkkkkkdddddddcaaaaaacccc
+abaaaacccccccccccccccccccccccccccaaccccccccccccccccccccccccccccccaacaaacccccccccccccfeekkkkkkkddddddcccaaaccccccc
+abcaaacccccccccccaaaccccccccaacccaaccccaaaaaccccaaaccccccccccccccaaccccccccccccccccceeeeekkkkdddddccccccaaccccccc
+abccccccccccccccaaaaaaccccccaaacaaccacaaaaaaaccaaaaccccccccccaccaaccccccccccccccccccceeeeeeeedddacccccccccccccccc
+abccccccccccccccaaaaaacccccccaaaaacaaaaaccaaaaaaaacccccccccccaaaaacccccccccccccccccccceeeeeeedaaacccccccccccccaaa
+abccccccaaacccccaaaaacccccccaaaaaacaaaaaaaaaaaaaaaccccccccccccaaaaaccccccccccccccccccccceeeeecaaacccccccccccccaaa
+abccccccaaaccccccaaaaacccccaaaaaaaaccaaaaacaaaaaaccccccccccccaaaaaacccccccccccccccccccccaaaccccaccccccccccccccaaa
+abccccaacaaaaacccaaaaacccccaaaaaaaacaaaaaaaaaaaaaaaccccaaaaccaaaacccccccccccccccccccccccaccccccccccccccccccaaaaaa
+abccccaaaaaaaaccccccccccccccccaaccccaacaaaaaaaaaaaaaaccaaaaccccaaacccccccccccccccccccccccccccccccccccccccccaaaaaa"#;
